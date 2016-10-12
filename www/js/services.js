e38b9d653da6c753e5cf23fae584185a519305a9
BBlanchard06 <benjamin.blanchard.work@gmail.com>
2016-10-12 17:00:25 +0200

    Mobile 1

--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,50 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Shows', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
+  var shows = [
+  {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    title: 'Mon premier show',
+    location: 'Paris',
+    description: 'Cool, amateur',
+    capacity: '10',
+    price: '10',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Salle_Pleyel_5.jpg/220px-Salle_Pleyel_5.jpg',
+    date: '2016-10-30',
+    style_id: 'Classique'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    title: 'Sebastien Tellier',
+    location: 'Lyon',
+    description: 'Intense',
+    capacity: '15',
+    price: '50',
+    image: "http://www.gqmagazine.fr/uploads/images/201421/cc/l_aventura_de_s__bastien_tellier_7651.jpeg",
+    date: '2016-10-19',
+    style_id: 'Electro'
   }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+ id: 3,
+    title: 'JazzyBazz',
+    location: 'Paris',
+    description: 'Ghetto',
+    capacity: '50',
+    price: '30',
+    image: "http://www.sortirenprovence.com/wp-content/uploads/2016/03/jazzy-bazz-465x620.jpg",
+    date: '2016-11-01',
+    style_id: 'Rap'
   }];
 
   return {
     all: function() {
-      return chats;
+      return shows;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(showId) {
+      for (var i = 0; i < shows.length; i++) {
+        if (shows[i].id === parseInt(showId)) {
+          return shows[i];
         }
       }
       return null;
