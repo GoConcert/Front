eae9544d85309595d97e330ea10c4170b606e870
BBlanchard06 <benjamin.blanchard.work@gmail.com>
2016-10-12 19:13:49 +0200

    Mobile 2

--- www/js/services.js
+++ www/js/services.js
@@ -1,46 +1,20 @@
 angular.module('starter.services', [])
 
-.factory('Shows', function() {
+.factory('Shows', function($http) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var shows = [
-  {
-    id: 1,
-    title: 'Mon premier show',
-    location: 'Paris',
-    description: 'Cool, amateur',
-    capacity: '10',
-    price: '10',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Salle_Pleyel_5.jpg/220px-Salle_Pleyel_5.jpg',
-    date: '2016-10-30',
-    style_id: 'Classique'
-  }, {
-    id: 2,
-    title: 'Sebastien Tellier',
-    location: 'Lyon',
-    description: 'Intense',
-    capacity: '15',
-    price: '50',
-    image: "http://www.gqmagazine.fr/uploads/images/201421/cc/l_aventura_de_s__bastien_tellier_7651.jpeg",
-    date: '2016-10-19',
-    style_id: 'Electro'
-  }, {
- id: 3,
-    title: 'JazzyBazz',
-    location: 'Paris',
-    description: 'Ghetto',
-    capacity: '50',
-    price: '30',
-    image: "http://www.sortirenprovence.com/wp-content/uploads/2016/03/jazzy-bazz-465x620.jpg",
-    date: '2016-11-01',
-    style_id: 'Rap'
-  }];
+  var shows = [];
 
   return {
     all: function() {
-      return shows;
+     return $http.get("https://api-shows-tonight.herokuapp.com/shows.json")
+        .then(function(response) {
+          shows = response.data;
+          return shows;
+        })
     },
+   
     get: function(showId) {
       for (var i = 0; i < shows.length; i++) {
         if (shows[i].id === parseInt(showId)) {
@@ -48,6 +22,12 @@ angular.module('starter.services', [])
         }
       }
       return null;
+    },
+    book: function(showId, user_name, nb_people) {
+      return $http.post("https://api-shows-tonight.herokuapp.com/shows/" + showId + "/book.json", {booking: {user_name: user_name, nb_people: nb_people}}).then(function(response){
+        booking = response.data;
+        return booking;
+      });
     }
-  };
+   };
 });
