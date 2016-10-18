5abfa129eb80e3d14aec57d6db03f2e05dd73580
BBlanchard06 <benjamin.blanchard.work@gmail.com>
2016-10-18 16:10:06 +0200

    Ben V2

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('starter.services', [])
 
-.factory('Shows', function($http) {
+.factory('Shows', function($http, $stateParams) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
@@ -8,12 +8,12 @@ angular.module('starter.services', [])
 
   return {
     all: function() {
-     return $http.get("http://goco.herokuapp.com/concerts.json")
-        .then(function(response) {
-          shows = response.data;
-          return shows;
-        })
-    },
+    return $http.get("http://goco.herokuapp.com/concerts.json")
+       .then(function(response) {
+         shows = response.data;
+         return shows;
+       })
+                },
     get: function(showId) {
       for (var i = 0; i < shows.length; i++) {
         if (shows[i].id === parseInt(showId)) {
@@ -21,7 +21,7 @@ angular.module('starter.services', [])
         }
       }
       return null;
-    },
+                },
     book: function(showId, user_name, nb_people) {
       return $http.post("http://goco.herokuapp.com/concerts/" + showId + "/reservation.json", {booking: {user_name: user_name, nb_people: nb_people}}).then(function(response){
         booking = response.data;
@@ -29,5 +29,5 @@ angular.module('starter.services', [])
       });
     },
    };
-x})
+})
 
